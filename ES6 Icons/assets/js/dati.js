2700a60a17bddd3a8e063b91d8a15477e3d57bd3
Federico <federicofigini90@gmail.com>
2021-03-22 19:12:27 +0100

    finito

--- ES6 Icons/assets/js/dati.js
+++ ES6 Icons/assets/js/dati.js
@@ -1,21 +1,3 @@
-// Milestone 1
-// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le 
-// icone disponibili come da layout.
-// Milestone 2
-// Coloriamo le icone per tipo.
-// Milestone 3 (BONUS)
-// Creiamo una select con i tipi di icone e usiamola per filtrare le icone
-
-$(document).ready(function() {
-    showIcon(orderIcons)
-
-    $("select").change(function(e) {
-        var userSelect = $(this).val();
-        filter(userSelect)
-    });
-});
-
-
 const icons = [{
         name: 'cat',
         prefix: 'fa-',
@@ -113,46 +95,3 @@ const icons = [{
         family: 'fas'
     }
 ];
-
-
-let animals = icons.filter((el) => el.type == "animal")
-let users = icons.filter((el) => el.type == "user")
-let vegetables = icons.filter((el) => el.type == "vegetable")
-const orderIcons = [...animals, ...users, ...vegetables]
-
-function showIcon(type) {
-
-    let iconsEl = $("#icons")
-
-    iconsEl.html("")
-
-    type.forEach(el => {
-        let color;
-        if (el.type == "animal") {
-            color = "blue"
-        } else if (el.type == "vegetable") {
-            color = "orange"
-        } else {
-            color = "purple"
-        }
-        iconsEl.append(`<li><i class="${el.family} ${el.prefix}${el.name} ${color}"></i> <span>${el.name}</span></li>`);
-    });
-}
-
-function filter(x) {
-
-    switch (x) {
-        case "animals":
-            showIcon(animals);
-            break;
-        case "vegetables":
-            showIcon(vegetables);
-            break;
-        case "users":
-            showIcon(users);
-            break;
-        default:
-            showIcon(orderIcons);
-            break;
-    }
-}
